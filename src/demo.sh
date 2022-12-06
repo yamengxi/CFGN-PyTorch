f3e68aa076008739c5b1ac3fb4087fbe433442d5
yamengxi <854341266@qq.com>
2022-12-06 22:50:47 +0800

    new development

--- src/demo.sh
+++ src/demo.sh
@@ -266,9 +266,22 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python -u main.py --n_GPUs 4 --n_threads 8 \
 
 
 # train CFGN scale = 2
-CUDA_VISIBLE_DEVICES=0,1,4,5 python -u main.py --n_GPUs 4 --n_threads 8 \
+CUDA_VISIBLE_DEVICES=0,1 python -u main.py --n_GPUs 2 --n_threads 4 \
 --model CFGN --scale 2 --n_feats 64 --n_resgroups 9 --act lrelu --block_type CFGM_v2 --dilation 3 \
---save CFGN_CFGM_v2+ACT_BIX2_F64R9 --data_test Set5+Set14+B100+Urban100+Manga109 --batch_size 64 --patch_size 128 --save_results --save_models --lr 0.0005 --decay 200-400-600-800-1000-1200 --epochs 0 # --test_every 0
+--save CFGN_CFGM_v2+ACT_BIX2_F64R9 --data_test Set5+Set14+B100+Urban100+Manga109 --batch_size 64 --patch_size 128 --save_results --save_models \
+--optimizer ADAM --lr 0.0005 --decay 100-200-300-400-500-600 --epochs 0 # --test_every 0
+
+CUDA_VISIBLE_DEVICES=2,3 python -u main.py --n_GPUs 2 --n_threads 4 \
+--model CFGN --scale 2 --n_feats 64 --n_resgroups 9 --act lrelu --block_type CFGM_v2 --dilation 3 \
+--save CFGN_CFGM_v2+ACT_BIX2_F64R9 --data_test Set5+Set14+B100+Urban100+Manga109 --batch_size 128 --patch_size 128 --save_results --save_models \
+--optimizer ADAM --lr 0.001 --decay 100-200-300-400-500-600 --epochs 0 # --test_every 0
+
+CUDA_VISIBLE_DEVICES=0,1 python -u main.py --n_GPUs 2 --n_threads 4 \
+--model CFGN --scale 2 --n_feats 64 --n_resgroups 9 --act lrelu --block_type CFGM_v2 --dilation 3 --pre_train ??? \
+--save CFGN_CFGM_v2+ACT_BIX2_F64R9 --data_test Set5+Set14+B100+Urban100+Manga109 --batch_size ??? --patch_size 128 --save_results --save_models \
+--optimizer SGD --lr 0.01 --decay 200-400-600-800-1000-1200 --epochs 0 # --test_every 0
+
+
 
 # train CFGN scale = 3
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -u main.py --n_GPUs 4 --n_threads 8 \
